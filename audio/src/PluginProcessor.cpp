d8debf50f28bf1a1a26e3fc599dc254ff4013344
Marc Halbrügge <marc.halbruegge@tu-berlin.de>
2015-11-24 12:12:46 +0100

    added host parameter automation

--- audio/src/PluginProcessor.cpp
+++ audio/src/PluginProcessor.cpp
@@ -18,8 +18,16 @@
 //==============================================================================
 PluginAudioProcessor::PluginAudioProcessor()
 {
+    addParameter(new HostParam<Param>(osc1fine));
+    addParameter(new HostParam<Param>(osc1coarse));
+
     addParameter(new HostParam<Param>(lfo1freq));
     addParameter(new HostParam<Param>(osc1lfo1depth));
+
+    addParameter(new HostParam<Param>(osc1trngAmount));
+    addParameter(new HostParam<Param>(osc1pulsewidth));
+
+    addParameter(new HostParam<Param>(panDir));
 }
 
 PluginAudioProcessor::~PluginAudioProcessor()
