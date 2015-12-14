66bd5bfdc2dd70c855a76285421ce97049b75bdc
Luc Guy <luc.guy@polytech.univ-montp2.fr>
2015-12-14 22:21:23 +0100

    Uncommitted changes from merge

--- audio/inc/Param.h
+++ audio/inc/Param.h
@@ -6,13 +6,15 @@
 
 class Param {
 public:
-    Param(const String &name, const String &unit, float minval, float maxval, float defaultval)
+    Param(const String &name, const String &serializationTag, const String &unit, float minval, float maxval, float defaultval, int numSteps=0)
     : val_(defaultval)
     , min_(minval)
     , max_(maxval)
     , default_(defaultval)
     , name_(name)
+    , serializationTag_(serializationTag)
     , unit_(unit)
+    , numSteps_(numSteps)
     {
         jassert(minval < maxval);
         jassert(minval <= defaultval);
@@ -21,17 +23,23 @@ public:
     virtual ~Param() {}
 
     const String& name() const { return name_; }
+    const String& serializationTag() const { return serializationTag_; }
     const String& unit() const { return unit_; }
+    int getNumSteps() const { return numSteps_; }
 
     void set(float f) { val_.store(f); }
     float get() const { return val_.load(); }
 
-    //! \todo check min/max!
+
     virtual void setUI(float f, bool notifyHost = true) {
-        set(f);
+        if (f >= min_ && f <= max_) set(f);
+        else set(default_);
         if(notifyHost) listener.call(&Listener::paramUIChanged);
     }
     virtual float getUI() const { return get(); }
+    virtual String getUIString() const { return getUIString(get()); }
+    virtual String getUIString(float v) const { return String::formatted("%f", v); }
+    virtual bool hasLabels() const { return false; }
 
     float getMin() const { return min_; }
     float getMax() const { return max_; }
@@ -73,7 +81,9 @@ protected:
     float max_;
     float default_;
     String name_;
+    String serializationTag_;
     String unit_;
+    int numSteps_;
 
     ListenerList<Listener> listener;
     std::atomic<bool> uiDirty;
@@ -83,10 +93,63 @@ class ParamDb : public Param {
 public:
     using Param::Param;
 
-    //! \todo check min/max!
     virtual void setUI(float f, bool notifyHost = true) override {
-        set(fromDb(f));
+        if (fromDb(f) >= min_ && fromDb(f) <= max_) set(fromDb(f));
+        else set(default_);
         if (notifyHost) listener.call(&Listener::paramUIChanged);
     }
     virtual float getUI() const override { return toDb(get()); }
 };
+
+template<typename _enum>
+class ParamStepped : public Param {
+public:
+    ParamStepped(const String &name, const String &serializationTag, _enum defaultval, const char **labels = nullptr)
+    : Param(name, serializationTag, "", 0.f, static_cast<float>(_enum::nSteps)-1.f,
+            static_cast<float>(defaultval),
+            static_cast<int>(_enum::nSteps))
+    , step_(defaultval)
+    , labelsSet(false)
+    {
+        const char **lbl = labels;
+        for(size_t u = 0; u<labels_.size() && lbl != nullptr && *lbl != nullptr; ++u, ++lbl) {
+            labels_[u] = *lbl;
+            labelsSet = true;
+        }
+    }
+
+    //! \todo what about the set method? Make sure that no inconsistent state is created!
+    
+    _enum getStep () const {
+        return step_.load();
+    }
+    void setStep (_enum v) {
+        step_.store(v);
+        set(static_cast<float>(v));
+    }
+    
+    virtual void setUI(float f, bool notifyHost = true) override {
+        set(f);
+        int ival = static_cast<int>(std::trunc(f + .5f));
+        jassert(ival >= 0 && ival < static_cast<int>(_enum::nSteps));
+        step_.store(static_cast<_enum>(ival));
+        if (notifyHost) listener.call(&Listener::paramUIChanged);
+    }
+    virtual String getUIString() const override { return labels_[static_cast<size_t>(getStep())]; }
+    virtual String getUIString(float v) const override {
+        size_t u = static_cast<size_t>(std::trunc(v+.5f));
+        if(u<labels_.size()) {
+            return labels_[u];
+        } else {
+            jassert(false);
+            return String::formatted("val%u",u);
+        }
+    }
+    virtual bool hasLabels() const override { return labelsSet; }
+
+protected:
+    std::atomic<_enum> step_;
+    std::array<String, static_cast<size_t>(_enum::nSteps)> labels_;
+    bool labelsSet;
+};
+
