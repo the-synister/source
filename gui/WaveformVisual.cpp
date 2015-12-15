a8140c48c2286a7b4599a113b3cb084d60a3b09e
Xavier <xavier.presser@googlemail.com>
2015-12-15 12:28:39 +0100

    waveform starts now from the center of the y-axis

--- gui/WaveformVisual.cpp
+++ gui/WaveformVisual.cpp
@@ -9,8 +9,9 @@ void WaveformVisual::paint(Graphics &g)
 	const float amplitude = 0.4f;
 	const int width = getWidth();
 	const float step = 4*float_Pi / width;
+	wavePath.startNewSubPath(0, centreY);
 
-	for (float x = 0.0f; x < width; ++x) {
+	for (float x = 1.0f; x < width; ++x) {
 
 		float phs = x * step;
 		if (phs > (2 * float_Pi))
