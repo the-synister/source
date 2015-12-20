690b7f60714e4d276778741a02ac8ec2fea3e41b
jessen.nils <nils.jessen@posteo.de>
2015-12-20 12:14:09 +0100

    added files for realzies this time

---
+++ audio/inc/ModManager.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ModManager.h
+    Created: 20 Dec 2015 12:12:07pm
+    Author:  nj
+
+  ==============================================================================
+*/
+
+#ifndef MODMANAGER_H_INCLUDED
+#define MODMANAGER_H_INCLUDED
+
+
+
+
+
+#endif  // MODMANAGER_H_INCLUDED
