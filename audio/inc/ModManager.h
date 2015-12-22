061bf2d9dd605440bd5aec095d7ea24f13ac3b55
jessen.nils <nils.jessen@posteo.de>
2015-12-22 23:12:40 +0100

    concept

--- audio/inc/ModManager.h
+++ audio/inc/ModManager.h
@@ -13,12 +13,17 @@
 
 // give parameters a mod option: bipolar, unipolar (direction), none
 
+// create an efficient audiobuffer structure
+
 // init()
 // iterate over all mod sources and add them to the manager, sort by mod type
 // *dont forget the midi input*
 
 // convert ranges (e.g. midi -> bipolar)
 
-// connect mod source and target (dynamically)
+// connect mod source and target initially
+
+// possibility to reconnect source / target dynamically
+// set ranges (100%) dynamically according to knob position
 
 #endif  // MODMANAGER_H_INCLUDED
