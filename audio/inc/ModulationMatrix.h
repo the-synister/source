13f4b4de585a99e89030d058d79c7b94def1ee6b
nj <nils.jessen@posteo.de>
2016-01-03 00:42:01 +0100

    core functions added, sources and destinations in progress

--- audio/inc/ModulationMatrix.h
+++ audio/inc/ModulationMatrix.h
@@ -18,56 +18,199 @@
 "Designing Software Synthesizer Plug-Ins in C++"
 */
 
-// modulation sources
-enum class sources {
-	sNone = 0,
+enum sources {
+	SOURCE_NONE = 0,
 
-	// midi sources
-	sMidiNote,
-	sMidiVel,
-	sMidiModW,
-	sMidiPbW,
+	SOURCE_UI_OSC1_FO,
+	SOURCE_UI_FILTER1_FC,
 
-	// internal modulators
-	sLfo1,
-	sEnv1,
-	maxSources
+	SOURCE_LFO1,
+	SOURCE_ENV1,
+	SOURCE_VELOCITY,
+	SOURCE_MODWHEEL,
+	SOURCE_PITCHBEND,
+	MAX_SOURCES
 };
 
-// modulation destinations
-enum class destinations {
-	dNone = 0,
+enum destinations {
+	DEST_NONE = 0,
 
-	// layer 0
-	dFilterF,
+	// --- GUI control layer modulation (-1)
+	DEST_UI_OSC1_FO,
+	DEST_UI_OSC2_FO,
+	DEST_UI_TO_OSC3_FO,
 
-	// add more layers here
-	// e.g layer 1 
-	// dLfo1F (internal mod source to internal mod source)
-	// or allLfoF (universal destinations)
+	DEST_UI_FILTER1_FC,
+	DEST_UI_FILTER2_FC,
 
-	maxDestinations
+	// --- LAYER 0 DESTINATIONS 
+	//     add more L0 destinations in this chunk
+	//     see CModulationMatrix::checkDestinationLayer())
+
+	DEST_LFO1_FO, // <- keep this first
+	DEST_LFO2_FO, // <- keep this last
+
+				  // --- END OF LAYER 0 DESTINATIONS
+
+				  // --- LAYER 1 DESTINATIONS 
+				  //	DEST_OSC1_PULSEWIDTH,
+
+				  // --- END OF LAYER 1 DESTINATIONS
+
+	MAX_DESTINATIONS
 };
 
-// transformations
-enum class transform {
-	tNone = 0,
-	tUniToBi,
-	tBiToUni,
-	tMidiNorm,
-	tMidiNormInv,
-	tMidiToBi,
-	tMidiToUni,
-	tMidiSwitch,
-	maxTransforms
+enum transform {
+	TRANSFORM_NONE = 0,
+	TRANSFORM_UNIPOLAR_TO_BIPOLAR,
+	TRANSFORM_BIPOLAR_TO_UNIPOLAR,
+	TRANSFORM_MIDI_NORMALIZE,
+	TRANSFORM_INVERT_MIDI_NORMALIZE,
+	TRANSFORM_MIDI_TO_BIPOLAR,
+	TRANSFORM_MIDI_TO_PAN,
+	TRANSFORM_MIDI_SWITCH,
+	TRANSFORM_MIDI_TO_ATTENUATION,
+	TRANSFORM_NOTE_NUMBER_TO_FREQUENCY,
+	MAX_TRANSFORMS /* not needed? */
+};
+
+const float midiFreqTable[128] = {
+	8.1757993698120117,
+	8.6619567871093750,
+	9.1770238876342773,
+	9.7227182388305664,
+	10.3008613586425780,
+	10.9133825302124020,
+	11.5623254776000980,
+	12.2498569488525390,
+	12.9782714843750000,
+	13.7500000000000000,
+	14.5676174163818360,
+	15.4338531494140630,
+	16.3515987396240230,
+	17.3239135742187500,
+	18.3540477752685550,
+	19.4454364776611330,
+	20.6017227172851560,
+	21.8267650604248050,
+	23.1246509552001950,
+	24.4997138977050780,
+	25.9565429687500000,
+	27.5000000000000000,
+	29.1352348327636720,
+	30.8677062988281250,
+	32.7031974792480470,
+	34.6478271484375000,
+	36.7080955505371090,
+	38.8908729553222660,
+	41.2034454345703130,
+	43.6535301208496090,
+	46.2493019104003910,
+	48.9994277954101560,
+	51.9130859375000000,
+	55.0000000000000000,
+	58.2704696655273440,
+	61.7354125976562500,
+	65.4063949584960940,
+	69.2956542968750000,
+	73.4161911010742190,
+	77.7817459106445310,
+	82.4068908691406250,
+	87.3070602416992190,
+	92.4986038208007810,
+	97.9988555908203130,
+	103.8261718750000000,
+	110.0000000000000000,
+	116.5409393310546900,
+	123.4708251953125000,
+	130.8127899169921900,
+	138.5913085937500000,
+	146.8323822021484400,
+	155.5634918212890600,
+	164.8137817382812500,
+	174.6141204833984400,
+	184.9972076416015600,
+	195.9977111816406200,
+	207.6523437500000000,
+	220.0000000000000000,
+	233.0818786621093700,
+	246.9416503906250000,
+	261.6255798339843700,
+	277.1826171875000000,
+	293.6647644042968700,
+	311.1269836425781200,
+	329.6275634765625000,
+	349.2282409667968700,
+	369.9944152832031200,
+	391.9954223632812500,
+	415.3046875000000000,
+	440.0000000000000000,
+	466.1637573242187500,
+	493.8833007812500000,
+	523.2511596679687500,
+	554.3652343750000000,
+	587.3295288085937500,
+	622.2539672851562500,
+	659.2551269531250000,
+	698.4564819335937500,
+	739.9888305664062500,
+	783.9908447265625000,
+	830.6093750000000000,
+	880.0000000000000000,
+	932.3275146484375000,
+	987.7666015625000000,
+	1046.5023193359375000,
+	1108.7304687500000000,
+	1174.6590576171875000,
+	1244.5079345703125000,
+	1318.5102539062500000,
+	1396.9129638671875000,
+	1479.9776611328125000,
+	1567.9816894531250000,
+	1661.2187500000000000,
+	1760.0000000000000000,
+	1864.6550292968750000,
+	1975.5332031250000000,
+	2093.0046386718750000,
+	2217.4609375000000000,
+	2349.3181152343750000,
+	2489.0158691406250000,
+	2637.0205078125000000,
+	2793.8259277343750000,
+	2959.9553222656250000,
+	3135.9633789062500000,
+	3322.4375000000000000,
+	3520.0000000000000000,
+	3729.3100585937500000,
+	3951.0664062500000000,
+	4186.0092773437500000,
+	4434.9218750000000000,
+	4698.6362304687500000,
+	4978.0317382812500000,
+	5274.0410156250000000,
+	5587.6518554687500000,
+	5919.9106445312500000,
+	6271.9267578125000000,
+	6644.8750000000000000,
+	7040.0000000000000000,
+	7458.6201171875000000,
+	7902.1328125000000000,
+	8372.0185546875000000,
+	8869.8437500000000000,
+	9397.2724609375000000,
+	9956.0634765625000000,
+	10548.0820312500000000,
+	11175.3037109375000000,
+	11839.8212890625000000,
+	12543.8535156250000000
 };
 
 struct modMatrixRow
 {
 	uint8 sourceIndex;
 	uint8 destinationIndex;
-	float* modIntensity;
-	float* modRange;
+	double* modIntensity;
+	double* modRange;
 	uint8 sourceTransform;
 	bool enable;
 };
@@ -75,8 +218,8 @@ struct modMatrixRow
 
 inline modMatrixRow* createModMatrixRow(uint8 sourceIndex_,
 										uint8 destinationIndex_,
-										float* modIntensity_,
-										float* modRange_,
+										double* modIntensity_,
+										double* modRange_,
 										uint8 sourceTransform_,
 										bool enable_ = true) {
 
@@ -91,6 +234,13 @@ inline modMatrixRow* createModMatrixRow(uint8 sourceIndex_,
 	return row;
 }
 
+inline modMatrixRow** createModMatrixCore()
+{
+	modMatrixRow** core = new modMatrixRow*[MAX_SOURCES * MAX_DESTINATIONS];
+	memset(core, 0, MAX_SOURCES * MAX_DESTINATIONS * sizeof(modMatrixRow*));
+	return core;
+}
+
 // core
 class ModulationMatrix {
 public:
@@ -100,23 +250,32 @@ public:
 	modMatrixRow** getModMatrixCore();
 	void setModMatrixCore(modMatrixRow** modMatrix);
 	uint8 getMatrixSize();
-	void clearMatrix();
-	void clearSources();
-	void clearDestinations();
-	void addModMatrixRow(modMatrixRow*);
-	bool modMatrixRowExists(modMatrixRow*);
-	void createMatrixCore();
-	void deleteModMatrix();
-	void enableModMatrixRow();
-	void checkDestinationLayer();
-	void doModulationsMatrix();
+	inline void clearMatrix();
+	inline void clearSources();
+	inline void clearDestinations();
+	inline void addModMatrixRow(modMatrixRow* row);
+	inline bool modMatrixRowExists(uint8 sourceIndex, uint8 destinationIndex);
+	inline bool createMatrixCore();
+	inline void deleteModMatrix();
+	inline bool enableModMatrixRow(uint8 sourceIndex, uint8 destinationIndex, bool enable);
+	inline bool checkDestinationLayer(uint8 layer, modMatrixRow* row);
+	inline void doModulationsMatrix(uint8 modLayer);
 
-	float sources[static_cast<uint8>(sources::maxSources)];
-	float destinations[static_cast<uint8>(destinations::maxDestinations)];
+	double sources[MAX_SOURCES];
+	double destinations[MAX_DESTINATIONS];
 
 protected:
+	inline double mmaMIDItoAtten(uint8 uMIDIValue);
+	inline double mmaMIDItoAtten_dB(uint8 uMIDIValue);
+	inline uint8 unipolarToMIDI(float fUnipolarValue);
+	inline double midiToUnipolar(uint8 uMIDIValue);
+	inline double bipolarToUnipolar(double dValue);
+	inline double midiToPanValue(uint8 uMIDIValue);
+	inline double midiToBipolar(uint8 uMIDIValue);
+	inline double unipolarToBipolar(double dValue);
+
 	modMatrixRow** matrixCore;
-	int size;
+	uint8 size;
 };
 
 #endif  // MODULATIONMATRIX_H_INCLUDED
--- audio/src/ModulationMatrix.cpp
+++ audio/src/ModulationMatrix.cpp
@@ -10,6 +10,21 @@
 
 #include "ModulationMatrix.h" 
 
+ModulationMatrix::ModulationMatrix(void)
+{
+	matrixCore = NULL;
+	createModMatrixCore();
+
+	size = 0;
+	clearMatrix(); // fill with NULL
+	clearSources();
+	clearDestinations();
+}
+
+ModulationMatrix::~ModulationMatrix(void)
+{
+}
+
 modMatrixRow ** ModulationMatrix::getModMatrixCore()
   {
 	  return matrixCore;
@@ -29,4 +44,334 @@ void ModulationMatrix::setModMatrixCore(modMatrixRow ** modMatrix)
 	}
 	matrixCore = modMatrix;
 	size = getMatrixSize();
+}
+
+uint8 ModulationMatrix::getMatrixSize()
+{
+	uint8 nSize = 0;
+	if (!matrixCore) return nSize;
+
+	for (int i = 0; i<MAX_SOURCES *
+		MAX_DESTINATIONS; i++)
+	{
+		modMatrixRow* row = matrixCore[i];
+		if (row)
+			nSize++;
+	}
+	return nSize;
+}
+
+// config changes
+inline void ModulationMatrix::clearMatrix()
+{
+	if (!matrixCore) return;
+
+	for (int i = 0; i<(MAX_SOURCES*MAX_DESTINATIONS); i++)
+	{
+		matrixCore[i] = NULL;
+	}
+}
+
+inline void ModulationMatrix::clearSources()
+{
+	for (int i = 0; i<MAX_DESTINATIONS; i++)
+	{
+		sources[i] = 0.0;
+	}
+}
+
+inline void ModulationMatrix::clearDestinations()
+{
+	for (int i = 0; i<MAX_SOURCES; i++)
+	{
+		destinations[i] = 0.0;
+	}
+}
+
+inline void ModulationMatrix::addModMatrixRow(modMatrixRow * row)
+{
+	if (!matrixCore)
+		createMatrixCore();
+
+	// add if not already existing
+	if (!modMatrixRowExists(row->sourceIndex, row->destinationIndex))
+	{
+		matrixCore[size] = row;
+		size++;
+	}
+	else
+		delete row;
+}
+
+inline bool ModulationMatrix::modMatrixRowExists(uint8 sourceIndex, uint8 destinationIndex)
+{
+	if (!matrixCore) return false;
+
+	for (int i = 0; i<size; i++)
+	{
+		modMatrixRow* row = matrixCore[i];
+
+		// find matching source/destination pairs
+		if (row->sourceIndex == sourceIndex && row->destinationIndex == destinationIndex)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+inline bool ModulationMatrix::createMatrixCore()
+{
+	if (matrixCore)
+		delete[] matrixCore;
+
+	// --- dynamic allocation of matrix core
+	matrixCore = new modMatrixRow*[MAX_SOURCES *
+		MAX_DESTINATIONS];
+	memset(matrixCore, 0, MAX_SOURCES *
+		MAX_DESTINATIONS*sizeof(modMatrixRow*));
+}
+
+inline void ModulationMatrix::deleteModMatrix()
+{
+	if (!matrixCore) return;
+
+	for (int i = 0; i<size; i++)
+	{
+		// delete pointer
+		modMatrixRow* row = matrixCore[i];
+		delete row;
+		size--;
+	}
+	size = 0;
+	delete[] matrixCore;
+	matrixCore = NULL;
+}
+
+inline bool ModulationMatrix::enableModMatrixRow(uint8 sourceIndex, uint8 destinationIndex, bool enable)
+{
+	if (!matrixCore) return false;
+
+	for (int i = 0; i<size; i++)
+	{
+		modMatrixRow* row = matrixCore[i];
+
+		// find matching source/destination pairs
+		if (row->sourceIndex == sourceIndex && row->destinationIndex == destinationIndex)
+		{
+			row->enable = enable;
+			return true; // found it
+		}
+	}
+	return false;
+}
+
+inline bool ModulationMatrix::checkDestinationLayer(uint8 layer, modMatrixRow * row)
+{
+	bool bLayer0 = false;
+	if (row->destinationIndex >= DEST_LFO1_FO &&
+		row->destinationIndex <= DEST_LFO2_FO)
+		// then, we are layer 0; see modulationmatrix.h
+		bLayer0 = true;
+
+	if (layer == 0)
+		return bLayer0;
+
+	if (layer == 1)
+		return !bLayer0;
+
+	return false;
+}
+
+/* unipolarToBipolar()
+
+calculates the bipolar (-1 -> +1) value from a unipolar (0 -> 1) value
+
+dValue = the value to convert
+*/
+inline double ModulationMatrix::unipolarToBipolar(double dValue)
+{
+	return 2.0*dValue - 1.0;
+}
+
+/* midiToBipolar()
+
+calculates the bipolar (-1 -> +1) value from a unipolar MIDI (0 -> 127) value
+
+uint8 uMIDIValue = the MIDI value to convert
+*/
+inline double ModulationMatrix::midiToBipolar(uint8 uMIDIValue)
+{
+	return (2.0*static_cast<double>(uMIDIValue) / 127.0 - 1.0);
+}
+
+/* midiToPanValue()
+
+calculates the pan value (-1 -> +1) value from a unipolar MIDI (0 -> 127) value
+
+uint8 uMIDIValue = the MIDI value to convert
+*/
+inline double ModulationMatrix::midiToPanValue(uint8 uMIDIValue)
+{
+	// see MMA DLS Level 2 Spec; controls are asymmetrical
+	if (uMIDIValue == 64)
+		return 0.0;
+	else if (uMIDIValue <= 1) // 0 or 1
+		return -1.0;
+
+	return (2.0*static_cast<double>(uMIDIValue) / 127.0 - 1.0);
+}
+
+/* bipolarToUnipolar()
+
+calculates the unipolar (0 -> 1) value from a bipolar (-1 -> +1) value
+
+dValue = the value to convert
+*/
+inline double ModulationMatrix::bipolarToUnipolar(double dValue)
+{
+	return 0.5*dValue + 0.5;
+}
+
+/* midiToUnipolar()
+
+calculates the unipolar (0 -> 1) value from a MIDI (0 -> 127) value
+
+dValue = the value to convert
+*/
+inline double ModulationMatrix::ModulationMatrix::midiToUnipolar(uint8 uMIDIValue)
+{
+	return (double)uMIDIValue / 127.0;
+}
+
+/* unipolarToMIDI()
+
+calculates the MIDI (0 -> 127) value from a unipolar (0 -> 1) value
+
+dValue = the value to convert
+*/
+inline uint8 ModulationMatrix::unipolarToMIDI(float fUnipolarValue)
+{
+	return static_cast<uint8>(fUnipolarValue*127.0);
+}
+
+/* mmaMIDItoAtten_dB()
+
+calculates the dB of attenuation according to MMA DLS spec
+
+uMIDIValue = the MIDI (0 -> 127) value to convert
+*/
+inline double ModulationMatrix::mmaMIDItoAtten_dB(uint8 uMIDIValue)
+{
+	if (uMIDIValue == 0)
+		return -96.0; // dB floor
+
+	return 20.0*log10((127.0*127.0) / ((float)uMIDIValue*(float)uMIDIValue));
+}
+
+/* mmaMIDItoAtten()
+
+calculates the raw attenuation according to MMA DLS spec
+
+uMIDIValue = the MIDI (0 -> 127) value to convert
+*/
+inline double ModulationMatrix::mmaMIDItoAtten(uint8 uMIDIValue)
+{
+	if (uMIDIValue == 0)
+		return 0.0; // floor
+
+	return (static_cast<double>(uMIDIValue)*static_cast<double>(uMIDIValue)) / (127.0*127.0);
+}
+
+inline void ModulationMatrix::doModulationsMatrix(uint8 modLayer)
+{
+	if (!matrixCore) return;
+
+	// clear dest registers
+	clearDestinations();
+
+	for (int i = 0; i<size; i++)
+	{
+		// get the row
+		modMatrixRow* row = matrixCore[i];
+
+		// --- this should never happen!
+		if (!row) continue;
+
+		// --- if disabled, skip row
+		if (!row->enable) continue;
+
+		// --- check the mod layer
+		if (!checkDestinationLayer(modLayer, row)) continue;
+
+		// get the source value
+		double source = sources[row->sourceIndex];
+
+		switch (row->sourceTransform)
+		{
+		case TRANSFORM_UNIPOLAR_TO_BIPOLAR:
+			source = unipolarToBipolar(source);
+			break;
+
+		case TRANSFORM_BIPOLAR_TO_UNIPOLAR:
+			source = bipolarToUnipolar(source);
+			break;
+
+		case TRANSFORM_MIDI_TO_ATTENUATION:
+			source = mmaMIDItoAtten(source);
+			break;
+
+		case TRANSFORM_MIDI_TO_PAN:
+			source = midiToPanValue(source);
+			break;
+
+		case TRANSFORM_MIDI_SWITCH:
+			source = source > 63 ? 1.0 : 0.0;
+			break;
+
+		case TRANSFORM_MIDI_TO_BIPOLAR:
+			source = midiToBipolar(source);
+			break;
+
+		case TRANSFORM_NOTE_NUMBER_TO_FREQUENCY:
+			source = midiFreqTable[(uint8)source];
+			break;
+
+		case TRANSFORM_MIDI_NORMALIZE:
+			source /= 127.0; // 0->1 NOTE: MMA DLS uses divide-by-128 instead!, 0->0.9999
+			break;
+
+		case TRANSFORM_INVERT_MIDI_NORMALIZE:
+			source /= 127.0; // 0->1 NOTE: MMA DLS uses divide-by-128 instead!, 0->0.9999
+			source = 1.0 - source; // 1->0 NOTE: MMA DLS uses divide-by-128 instead!, 0.9999->0
+			break;
+
+		default:
+			break;
+		}
+
+		// destination += source*intensity*range
+		//
+		double dModValue = source*(*row->modIntensity)*(*row->modRange);
+
+		destinations[row->destinationIndex] += dModValue;
+
+		// universal connections example:
+		// first check DEST_ALL types
+		/*switch (row->destinationIndex)
+		{
+		
+		case DEST_ALL_OSC_FO:
+			m_dDestinations[DEST_OSC1_FO] += dModValue;
+			m_dDestinations[DEST_OSC2_FO] += dModValue;
+			m_dDestinations[DEST_OSC3_FO] += dModValue;
+			m_dDestinations[DEST_OSC4_FO] += dModValue;
+			m_dDestinations[DEST_ALL_OSC_FO] += dModValue;
+			break;
+
+			// for all "single" source/dest, this is the modulation
+		default:
+			destinations[row->destinationIndex] += dModValue;
+		}*/
+	}
 }
