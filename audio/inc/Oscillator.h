d46b515274466b466dec4cfa796d2e1d5f0a2a6c
schmied.anton <anton.schmied@hotmail.de>
2016-01-09 20:51:50 +0100

    added Oscillator .cpp and .h

---
+++ audio/inc/Oscillator.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    Oscillator.h
+    Created: 9 Jan 2016 8:48:22pm
+    Author:  Anton
+
+  ==============================================================================
+*/
+
+#ifndef OSCILLATOR_H_INCLUDED
+#define OSCILLATOR_H_INCLUDED
+
+
+
+
+
+#endif  // OSCILLATOR_H_INCLUDED
